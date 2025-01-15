0c47c3aa110580ff2231bda6302a444d54a74bfc
schunglu <sclu@ucdavis.edu>
2025-01-14 18:36:14 -0800

    update

--- 04spellingbee.sh
+++ 04spellingbee.sh
@@ -1,1 +1,1 @@
-gunzip -c ~/sophia/MCB185/data/dictionary.gz |grep "r" |grep -v "[^zonriac]" |grep -E ".{4,}"
+gunzip -c ~/Code/MCB185/data/dictionary.gz |grep "r" |grep -v "[^zonriac]" |grep -E ".{4,}"
