5fd0c0bedc2bbb66876b6eaca3778faee0486538
schunglu <sclu@ucdavis.edu>
2025-01-16 20:59:53 -0800

    test

--- 10demo.py
+++ 10demo.py
@@ -1,4 +1,10 @@
 # 10demo.py by schunglu
 
-print('hello, again') # greeting
+import math 
 
+print('hello, again') # greeting
+print (1.5e-5)
+print (1 + 1)
+print (2 // 6)
+print (math.pow(7, 9))
+print (math.sqrt(4))
